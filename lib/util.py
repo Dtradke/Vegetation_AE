f5ad339e5d567056bbe46f4ac644386350de26d0
Dave Radke <dtradke@uwaterloo.ca>
2020-04-20 22:18:59 -0700

    softmax UNET

--- lib/util.py
+++ lib/util.py
@@ -133,9 +133,9 @@ def formatPreds(pred):
     # pred[(pred >= 0.33) & (pred < 0.66)] = 0.5
     # pred[pred >= 0.66] = 1
     print(pred[0])
-    pred = np.amax(pred, axis=1)
-    print(pred[0])
-    return pred
+    max_pred = np.amax(pred, axis=1)
+    print(max_pred[0])
+    return max_pred
 
 def evaluateUNET(y_preds, masterDataSet):
     incorrect = 0
