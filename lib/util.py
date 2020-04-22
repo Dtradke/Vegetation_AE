c8334a3d01e8ea808d9358e9e183824c5302c702
Dave Radke <dtradke@uwaterloo.ca>
2020-04-21 23:39:55 -0700

    normal mod - debug

--- lib/util.py
+++ lib/util.py
@@ -170,13 +170,13 @@ def formatPreds(pred, val):
         pred[(pred >= 0.25) & (pred < .5)] = 1
         pred[(pred >= 0.5) & (pred < 0.66)] = 2
         pred[pred >= 0.66] = 3
-        print("before: ", val)
         val = np.squeeze(val)
+        print("before: ", val[0])
         val[val == 0] = 0
         val[val == 0.33] = 1
         val[val == 0.66] = 2
         val[val == 1] = 3
-        print("after: ", val)
+        print("after: ", val[0])
         return pred, val
 
 def evaluateUNET(y_preds, masterDataSet):
