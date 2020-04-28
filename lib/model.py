ce8910d51e5fad56c94043b7ace7c1d9fd2c3d28
Dave Radke <dtradke@uwaterloo.ca>
2020-04-28 11:36:21 -0700

    debugging on cluster

--- lib/model.py
+++ lib/model.py
@@ -209,8 +209,11 @@ def unet_mse(X_split_1, X_split_2, pretrained_weights = None):
     # drop4_x = K.dropout(x, 0.5, seed=1334)
 
     print(drop4_1)
+    tf.Print(drop4_1)
     print(drop4_2)
+    tf.Print(drop4_2)
     conc_4 = concatenate([drop4_1, drop4_2], axis=1)
+    tf.Print(conc_4)
     print(conc_4)
     conc_4 = Reshape((2, 512, 8, 8))(conc_4)
     dropout_layer = Dropout(rate=0.5, noise_shape=[None, 1, 2])(conc_4)
