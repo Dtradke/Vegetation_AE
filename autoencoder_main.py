c168c11e269276098c57a5e88b05ca344bb66d11
Dave Radke <dtradke@uwaterloo.ca>
2020-04-28 23:13:45 -0700

    unet-split long run early stop

--- autoencoder_main.py
+++ autoencoder_main.py
@@ -43,13 +43,13 @@ def getModelAndTrain(masterDataSet, mod, test_set):
             X_split_1, X_split_2 = masterDataSet.trainX[:,:,:,:4], masterDataSet.trainX[:,:,:,4:]
             val_split_1, val_split_2 = masterDataSet.valX[:,:,:,:4], masterDataSet.valX[:,:,:,4:]
             print("Split shape: ", X_split_1.shape, " ", X_split_2.shape)
-            mod = model.unet_mse(X_split_1, X_split_2)
+            mod = model.unet_split(X_split_1, X_split_2)
             inputs = [X_split_1, X_split_2]
             vals = [val_split_1, val_split_2]
 
-            # es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
-            # mc = ModelCheckpoint('models/split_drop_best_model.h5', monitor='val_accuracy', mode='max', verbose=1, save_best_only=True, save_weights_only=True)
-            mod.fit( inputs, masterDataSet.trainy, batch_size=32, epochs=30, verbose=1, validation_data=(vals, masterDataSet.valy)) #, callbacks=[es, mc]
+            es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
+            mc = ModelCheckpoint('models/split_nodrop_best_model.h5', monitor='val_accuracy', mode='max', verbose=1, save_best_only=True, save_weights_only=True)
+            mod.fit( inputs, masterDataSet.trainy, batch_size=32, epochs=300, verbose=1, validation_data=(vals, masterDataSet.valy, callbacks=[es, mc]))
         else:
             mod = model.unet(masterDataSet)
             mod.fit(masterDataSet.trainX, masterDataSet.trainy, batch_size=32, epochs=30, verbose=1, validation_data=(masterDataSet.valX, masterDataSet.valy))
