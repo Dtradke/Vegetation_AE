d5cc8872eabb1466e91456246abbce88b9f4e21f
Dave Radke <dtradke@uwaterloo.ca>
2020-04-28 18:17:04 -0700

    counting squares

--- lib/dataset.py
+++ lib/dataset.py
@@ -44,13 +44,19 @@ class Squares(object):
 
     def measureBal(self):
         total = self.square_labels[0].size
+        print(total)
         for i, square in enumerate(self.squares):
             footprint = np.count_nonzero(self.square_labels[i] == 0)
             grass = np.count_nonzero(self.square_labels[i] == 1)
             shrub = np.count_nonzero(self.square_labels[i] == 2)
             tree = np.count_nonzero(self.square_labels[i] == 3)
+            print(footprint)
+            print(grass)
+            print(shrub)
+            print(tree)
+            print()
 
-            print(i, " - foot: ", round((footprint/total), 4)," - grass: ", round((grass/total), 4)," - shrub: ", round((shrub/total), 4)," - tree: ", round((tree/total), 4))
+            # print(i, " - foot: ", round((footprint/total), 4)," - grass: ", round((grass/total), 4)," - shrub: ", round((shrub/total), 4)," - tree: ", round((tree/total), 4))
 
     def makeValDataset(self):
         l = int(self.trainX.shape[0] * 0.8)
