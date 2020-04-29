b77813e71a4c6e985c9898942500f71844d06f7e
Dave Radke <dtradke@uwaterloo.ca>
2020-04-28 20:52:52 -0700

    debugging

--- autoencoder_main.py
+++ autoencoder_main.py
@@ -27,6 +27,7 @@ def openDatasets(test_set, mod):
     data = []
     if mod is None:
         data = rawdata.RawData.load(locNames='all', special_layers='all')
+    # data.formatDataLayers()
     data.normalizeAllLayers()
     masterDataSet = dataset.Squares(data, test_set, mod)
     if not test_set: #its the test site
