c7240618fd25774b6d5e5eb53e09db15e6f41f48
Dave Radke <dtradke@uwaterloo.ca>
2020-05-09 14:09:24 -0700

    debug

--- autoencoder_main.py
+++ autoencoder_main.py
@@ -74,7 +74,7 @@ def openAndTrain(test_set=True, mod=None, load_datasets=False):
         masterDataSet = dataset.Squares(datasets=datasets)
     else:
         masterDataSet = openDatasets(test_set, mod)
-    test_len = (masterDataSet.trainX // masterDataSet.testX.shape[0])
+    test_len = (masterDataSet.trainX.shape[0] // masterDataSet.testX.shape[0])
     print("Length of tests: ", test_len)
     for i in range(test_len):
         print("Length of train: ", masterDataSet.trainX.shape[0], " and test: ", masterDataSet.testX.shape[0])
