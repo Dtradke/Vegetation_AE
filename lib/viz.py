c61fce97a6a9bca52ce2ddd7d544cba96ae8704c
Dave Radke <dtradke@uwaterloo.ca>
2020-05-31 11:30:23 -0700

    CDF

--- lib/viz.py
+++ lib/viz.py
@@ -67,18 +67,19 @@ def viewResult(layer, val, pred, diff, r_squared, num):
 
 def makeCDF(y_preds, ground, lower, upper):
     error = np.sort(np.absolute(np.subtract(ground, y_preds)))
+    error /= error.sum()
     cumsum_error = np.cumsum(error)
     x = np.linspace(0, cumsum_error.shape[0], cumsum_error.shape[0])
     plt.plot(x, cumsum_error, 'r')
-    plt.xlabel("Percent of Predictions (%)", fontsize=20)
-    plt.ylabel("Absolute Error (ft)", fontsize=20)
+    plt.ylabel("Percent of Predictions (%)", fontsize=20)
+    plt.xlabel("Absolute Error (ft)", fontsize=20)
     plt.title("CDF for " + str(lower) + " to " + str(upper) + " feet tall")
 
-    x_ticks = []
-    labels = []
-    for i in range(0,110,10):
-        x_ticks.append(int(x.shape[0] * (i/10)))
-        labels.append(str(i))
+    # x_ticks = []
+    # labels = []
+    # for i in range(0,110,10):
+    #     x_ticks.append(int(x.shape[0] * (i/10)))
+    #     labels.append(str(i))
 
     plt.xticks(x_ticks, labels)
 
