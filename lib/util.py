0a907a5ea5843fe354e767c1194edb45eb12c94a
Dave Radke <dtradke@uwaterloo.ca>
2020-05-31 17:55:23 -0700

    dev

--- lib/util.py
+++ lib/util.py
@@ -367,7 +367,8 @@ def calcError(y_preds, ground, lower=0, upper=2):
     y_preds = y_preds[(ground >= lower) & (ground < upper)]
     ground = ground[(ground >= lower) & (ground < upper)]
     rmse = np.sqrt(np.mean(np.square(np.subtract(ground, y_preds))))
-    print("lower: ", lower, " - upper: ", upper, " - rmse: ", rmse)
+    avg_abs = np.mean(np.absolute(np.subtract(ground, y_preds)))
+    print("lower: ", lower, " - upper: ", upper, " - rmse: ", rmse, " - avg_error_ft: ", avg_abs)
     return [y_preds, ground]
     # return rmse
 
