2bd03cb634ba2d8ce8849939ba7d8f4b1de5af01
Dave Radke <dtradke@uwaterloo.ca>
2020-06-06 14:39:26 -0700

    no dem

--- lib/util.py
+++ lib/util.py
@@ -391,16 +391,16 @@ def evaluateRegression(y_preds, masterDataSet):
             continue
         single_r_squareds.append(r)
 
-        if i < 500:
-            # viz.viewResult(masterDataSet.testX[i][:, :, -3], val, pred, absolute_diff, single_r_squareds[-1], i)
-            pred_squares.append(pred)
-            val_squares.append(val)
-            img_squares.append(masterDataSet.testX[i][:, :, -3])
-            viz.viewResultColorbar(masterDataSet.testX[i][:, :, -3], val, pred, absolute_diff, single_r_squareds[-1], i)
-
-    np.save('new_ynet_squares_pred.npy', np.array(pred_squares))
-    np.save('new_ynet_squares_ground.npy', np.array(val_squares))
-    np.save('new_ynet_squares_img.npy', np.array(img_squares))
+    #     if i < 500:
+    #         # viz.viewResult(masterDataSet.testX[i][:, :, -3], val, pred, absolute_diff, single_r_squareds[-1], i)
+    #         pred_squares.append(pred)
+    #         val_squares.append(val)
+    #         img_squares.append(masterDataSet.testX[i][:, :, -3])
+    #         viz.viewResultColorbar(masterDataSet.testX[i][:, :, -3], val, pred, absolute_diff, single_r_squareds[-1], i)
+    #
+    # np.save('new_ynet_squares_pred.npy', np.array(pred_squares))
+    # np.save('new_ynet_squares_ground.npy', np.array(val_squares))
+    # np.save('new_ynet_squares_img.npy', np.array(img_squares))
 
     # calculate result
     ground = masterDataSet.testy.flatten()
